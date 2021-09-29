b3752470f8e71c3e7459c340d140d3b6ae829986
vanloc1808 <vanloc1808@users.noreply.github.com>
2021-09-29 21:44:32 +0700

    first lesson of intro to R

---
+++ Introduction to R/Intro.R
@@ -0,0 +1,34 @@
+#1. Doi net ve R
+
+#2. Tai va cai dat R
+
+#3. Cau truc lenh chung trong R
+#3.1 Cau truc lenh trong R
+x <- c()
+example(sample)
+x <- sample(5)
+?sample
+help(sample)
+example(sample)
+
+#3.2 Mot so phep toan so sanh hay logic trong R
+x == 3
+x != 3
+x > 3
+x < 3
+x & 3
+x | 3
+!x
+xor(x, 3)
+
+#3.3 Cac ham toan hoc thuong dung trong R
+x <- 10
+log(x)
+log10(x)
+log(x, 3)
+exp(x)
+sqrt(x)
+factorial(x)
+choose(x, 4)
+floor(x)
+ceiling(x)
